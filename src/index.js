d4140fd44c5ab35fa4cafb32f3bea2d5db80aa8f
Racso1624 <fer10_lb@outlook.com>
2022-04-19 19:37:00 -0600

    Se agregan cosas de webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () =>{
     return <h1>Bievenido al Laberinto</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { dirname } = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/index.html")
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
   },
 };

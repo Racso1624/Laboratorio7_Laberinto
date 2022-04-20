f1399430f5e5db1da8320354381598d9babb6206
Racso1624 <fer10_lb@outlook.com>
2022-04-20 17:57:50 -0600

    Se hacen cambios

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ const creacion_Laberinto = async(ancho, alto) =>{
     const respuesta = await fetch('https://maze.juanelcaballo.club/?type=json&w=${ancho}&h=${alto}')
         .then(respuesta => respuesta.json())
         .then(datos => datos)
-    return respuesta 
+    return respuesta
+    .catch(error =>
+        console.log(error))
 }
 
 const modificar_Laberinto = async() =>{
     const laberinto_modificado = await creacion_Laberinto(ancho_laberinto, alto_laberinto)
 }
+
+
 
 const App = () =>{
     return <h1>Bievenido al Laberinto</h1>

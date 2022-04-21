eed25c17fc74170191dfb738c78833c689865114
Racso1624 <fer10_lb@outlook.com>
2022-04-21 00:21:14 -0600

    Se agregan cosas del laberinto

--- src/index.js
+++ src/index.js
@@ -5,18 +5,35 @@ const creacion_Laberinto = async(ancho, alto) =>{
     const respuesta = await fetch('https://maze.juanelcaballo.club/?type=json&w=${ancho}&h=${alto}')
         .then(respuesta => respuesta.json())
         .then(datos => datos)
+    console.log(respuesta)
     return respuesta
     .catch(error =>
         console.log(error))
 }
 
-const modificar_Laberinto = async() =>{
-    const laberinto_modificado = await creacion_Laberinto(ancho_laberinto, alto_laberinto)
-}
+
 
 
 
 const App = () =>{
+
+    const [laberinto, setLaberinto] = React.useState([])
+    const [ancho, setAncho] = React.useState(4)
+    const [alto, setAlto] = React.useState(4)
+
+    const modificar_Laberinto = async() =>{
+        const laberinto_modificado = await creacion_Laberinto(ancho_laberinto, alto_laberinto)
+        setLaberinto(laberinto_modificado)
+    }   
+
+    const actualizar_Laberinto = async(nuevoLaberinto) => {
+        modificar_Laberinto(nuevoLaberinto)
+    }
+
+    const modificar_Ancho = (valorAncho) =>{
+        
+    }
+
     return <h1>Bievenido al Laberinto</h1>
 }
 

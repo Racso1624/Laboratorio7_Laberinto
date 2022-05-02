68b0022e641a2c3897184d227091281d5b852588
Racso1624 <fer10_lb@outlook.com>
2022-05-02 15:59:16 -0600

    Se termina la funcionalidad

--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ const App = () =>{
     const [ancho, setAncho] = React.useState(4)
     const [alto, setAlto] = React.useState(4)
     const [cambioMedidas, setCambioMedidas] = React.useState(false)
+    const [partidaGanada, setPartidaGanada] = React.useState(false)
+
+    const header_estilo = css`
+        text-align: center;
+    `
 
     const tablero_estilo = css `
+        justify-content: center;
+        align-items: center;
         display: grid;
         grid-template-columns: repeat(${(parseInt(ancho) * 3) + 1}, 20px);
         grid-template-rows: repeat(${(parseInt(alto) * 2) + 1}, 20px);
@@ -68,9 +75,49 @@ const App = () =>{
         setCambioMedidas(true)
     }
 
+    const cambiar_posicion = (aumento_x, aumento_y) =>{
+
+        setLaberinto((posicion) => {
+            const posicion_temporal = [...posicion]
+            const posicion_y = posicion_temporal.findIndex((fila) => {
+                return fila.indexOf("p") > -1
+            })
+
+            const posicion_x = posicion_temporal[posicion_y].findIndex((columna) => {
+                return columna === "p"
+            })
+
+            if(posicion_temporal[posicion_y + aumento_y][posicion_x + aumento_x] === "g"){
+                setPartidaGanada(true)
+            }
+            else if(posicion_temporal[posicion_y + aumento_y][posicion_x + aumento_x] === "|" || posicion_temporal[posicion_y + aumento_y][posicion_x + aumento_x] === "+" || posicion_temporal[posicion_y + aumento_y][posicion_x + aumento_x] === "-"){
+
+            }
+            else{
+                posicion_temporal[posicion_y][posicion_x] = " "
+                posicion_temporal[posicion_y + aumento_y][posicion_x + aumento_x] = "p"
+            }
+
+            return posicion_temporal
+        })
+    }
+
     const movimiento = () =>{
 
         const tecla_presionada = event.key
+
+        if(tecla_presionada === "a"){
+            cambiar_posicion(-1, 0)
+        }
+        else if(tecla_presionada === "w"){
+            cambiar_posicion(0, -1)
+        }
+        else if(tecla_presionada === "d"){
+            cambiar_posicion(1, 0)
+        }
+        else if(tecla_presionada === "s"){
+            cambiar_posicion(0, 1)
+        }
 
     }
 
@@ -86,15 +133,26 @@ const App = () =>{
         modificar_Laberinto()
     }, [])
 
+    React.useEffect(() => {
+        if(partidaGanada === true){
+            alert("FELICIDADES HAS GANADO")
+            modificar_Laberinto()
+            setPartidaGanada(false)
+        }
+    }, [partidaGanada])
+
     window.onkeydown = movimiento
 
     return(
         <div className="app">
-            <h1>Bienvenido al Laberinto</h1>
-            <p>Elige las medidas del laberinto (Entre 1 y 10): </p>
-            Ancho<input type="number" id="ancho" min="1" max="10"></input>
-            Alto<input type="number" id="alto" min="1" max="10"></input>
-            <button onClick={cambiar_Medidas}>Generar</button>
+            <header css = {header_estilo}>
+                <h1>Bienvenido al Laberinto</h1>
+                <p>Elige las medidas del laberinto (Entre 1 y 10): </p>
+                Ancho <input type="number" id="ancho" min="1" max="10"></input>
+                Alto <input type="number" id="alto" min="1" max="10"></input>
+                <button onClick={cambiar_Medidas}>Generar</button>
+            </header>
+            <br></br>
             <div className="tablero" css = {tablero_estilo}>
                 {
                     laberinto.map((posiciony, y) =>
